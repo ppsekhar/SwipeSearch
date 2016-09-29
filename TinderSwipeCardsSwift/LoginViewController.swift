711a0e6623dd6ae6ab268cf531ecb8b7526aeaf4
Priyanka Sekhar <priyankasekhar@DN0a24691f.SUNet>
2016-09-28 17:38:42 -0700

    switched files to swfit

---
+++ TinderSwipeCardsSwift/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  TinderSwipeCardsSwift
+//
+//  Created by Priyanka Sekhar on 9/28/16.
+//  Copyright © 2016 gcweb. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+}
